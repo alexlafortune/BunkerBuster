ad81125085e3333e557b4607ae74fe9bad563ef3
Alex Lafortune <aplafortune@gmail.com>
2022-01-21 17:05:37 -0500

    FluidField rewritten to use class objects instead of ints to allow to future development.

--- Assets/Scripts/CAField.cs
+++ Assets/Scripts/CAField.cs
@@ -2,24 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FluidField
+public class CAField
 {
     private int width;
     private int height;
-    private List<int> nodes;
+    private List<CAType> nodes;
     private HashSet<int> changedNodes;
-    private List<int> sourceNodes;
-    private List<int> sinkNodes;
+    private List<int> sourceNodes, sinkNodes;
     private bool reverseStep;   // step in both directions to avoid biasing one direction
+    private CAType emptyNode, solidNode, fluidNode;
 
-    public FluidField(int width, int height)
+    public CAField(int width, int height)
     {
+        emptyNode = new CAType(CAPhysicsType.NONE);
+        solidNode = new CAType(CAPhysicsType.NONE);
+        fluidNode = new CAType(CAPhysicsType.FLOW);
+
         this.width = width;
         this.height = height;
-        nodes = new List<int>();
+        nodes = new List<CAType>();
 
         for (int i = 0; i < width * height; ++i)
-            nodes.Add(ParticleType.EMPTY);
+            nodes.Add(emptyNode);
 
         changedNodes = new HashSet<int>();
         sourceNodes = new List<int>();
@@ -27,7 +31,7 @@ public class FluidField
         reverseStep = false;
     }
 
-    public FluidField(Texture2D texture) : this(texture.width, texture.height)
+    public CAField(Texture2D texture) : this(texture.width, texture.height)
     {
         Init(texture);
     }
@@ -47,9 +51,9 @@ public class FluidField
         return new Vector2Int(i % width, i / width);
     }
 
-    private int GetParticleType(int x, int y)
+    private CAType GetNodeType(int x, int y)
     {
-        return IsOutOfBounds(x, y) ? ParticleType.SOLID : nodes[GetAddress(x, y)];
+        return IsOutOfBounds(x, y) ? solidNode : nodes[GetAddress(x, y)];
     }
 
     public void Init(Texture2D texture)
@@ -67,22 +71,22 @@ public class FluidField
                 Color color = texture.GetPixel(x, y);
 
                 if (color.DistanceTo(Color.black) < 0.1f)
-                    SetParticleType(x, y, ParticleType.SOLID);
+                    SetNodeType(x, y, solidNode);
                 else if (color.DistanceTo(Color.blue) < 0.1f)
-                    SetParticleType(x, y, ParticleType.FLUID);
+                    SetNodeType(x, y, fluidNode);
                 else if (color.DistanceTo(Color.green) < 0.1f)
                     sourceNodes.Add(GetAddress(x, y));
                 else if (color.DistanceTo(Color.red) < 0.1f)
                     sinkNodes.Add(GetAddress(x, y));
                 else
-                    SetParticleType(x, y, ParticleType.EMPTY);
+                    SetNodeType(x, y, emptyNode);
             }
         }
     }
 
     private bool IsEmpty(int x, int y)
     {
-        return GetParticleType(x, y) == ParticleType.EMPTY;
+        return GetNodeType(x, y) == emptyNode;
     }
 
     private bool IsOutOfBounds(int x, int y)
@@ -90,24 +94,24 @@ public class FluidField
         return x < 0 || x > width || y < 0 || y > height;
     }
 
-    private void MoveParticle(int x1, int y1, int x2, int y2)
+    private void MoveNode(int x1, int y1, int x2, int y2)
     {
         if (IsEmpty(x1, y1) || !IsEmpty(x2, y2))
             return;
 
-        SetParticleType(x2, y2, GetParticleType(x1, y1));
-        SetParticleType(x1, y1, ParticleType.EMPTY);
+        SetNodeType(x2, y2, GetNodeType(x1, y1));
+        SetNodeType(x1, y1, emptyNode);
     }
 
-    private void SetParticleType(int i, int type)
+    private void SetNodeType(int i, CAType type)
     {
         nodes[i] = type;
         changedNodes.Add(i);
     }
 
-    private void SetParticleType(int x, int y, int type)
+    private void SetNodeType(int x, int y, CAType type)
     {
-        SetParticleType(GetAddress(x, y), type);
+        SetNodeType(GetAddress(x, y), type);
     }
 
     public void Step()
@@ -116,14 +120,14 @@ public class FluidField
         {
             for (int x = reverseStep ? width - 1 : 0; reverseStep ? x >= 0 : x < width; x += reverseStep ? -1 : 1)
             {
-                int p = GetParticleType(x, y);
+                CAType p = GetNodeType(x, y);
 
-                if (p == ParticleType.EMPTY || p == ParticleType.SOLID)
+                if (p.PhysicsType == CAPhysicsType.NONE)
                     continue;
 
                 if (IsEmpty(x, y - 1))
                 {
-                    MoveParticle(x, y, x, y - 1);
+                    MoveNode(x, y, x, y - 1);
                 }
                 else
                 {
@@ -131,11 +135,11 @@ public class FluidField
 
                     if (IsEmpty(x + sign, y - 1))
                     {
-                        MoveParticle(x, y, x + sign, y - 1);
+                        MoveNode(x, y, x + sign, y - 1);
                     }
                     else if (IsEmpty(x - sign, y - 1))
                     {
-                        MoveParticle(x, y, x - sign, y - 1);
+                        MoveNode(x, y, x - sign, y - 1);
                     }
                     else if (IsEmpty(x + sign, y))
                     {
@@ -149,7 +153,7 @@ public class FluidField
                             ++i;
                         }
 
-                        MoveParticle(x, y, x + sign * i, y);
+                        MoveNode(x, y, x + sign * i, y);
                     }
                     else if (IsEmpty(x - sign, y))
                     {
@@ -163,17 +167,17 @@ public class FluidField
                             ++i;
                         }
 
-                        MoveParticle(x, y, x - sign * i, y);
+                        MoveNode(x, y, x - sign * i, y);
                     }
                 }
             }
         }
 
         foreach (int i in sourceNodes)
-            SetParticleType(i, ParticleType.FLUID);
+            SetNodeType(i, fluidNode);
 
         foreach (int i in sinkNodes)
-            SetParticleType(i, ParticleType.EMPTY);
+            SetNodeType(i, emptyNode);
 
         reverseStep = !reverseStep;
     }
@@ -193,9 +197,9 @@ public class FluidField
         {
             v = GetCoordinate(i);
 
-            if (nodes[i] == ParticleType.SOLID)
+            if (nodes[i] == solidNode)
                 c = Color.black;
-            else if (nodes[i] == ParticleType.FLUID)
+            else if (nodes[i] == fluidNode)
                 c = Color.blue;
             else
                 c = Color.white;
@@ -208,9 +212,20 @@ public class FluidField
     }
 }
 
-public static class ParticleType
+public class CAType
 {
-    public static readonly int EMPTY = 0;
-    public static readonly int SOLID = 1;
-    public static readonly int FLUID = 2;
+    public int PhysicsType { get; private set; }
+
+    public CAType(int physicsType)
+    {
+        PhysicsType = physicsType;
+    }
+}
+
+public static class CAPhysicsType
+{
+    public static readonly int NONE = 0;
+    public static readonly int FALL = 1;
+    public static readonly int PILE = 2;
+    public static readonly int FLOW = 3;
 }
--- Assets/Scripts/CAManager.cs
+++ Assets/Scripts/CAManager.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
-public class FluidManager : MonoBehaviour
+public class CAManager : MonoBehaviour
 {
-    private FluidField ff;
+    private CAField ff;
     private Texture2D texture;
     private float timer;
 
     private void Start()
     {
         Texture2D initTexture = Resources.Load<Texture2D>("sprites/ff_init_small");
-        ff = new FluidField(initTexture);
+        ff = new CAField(initTexture);
 
         texture = new Texture2D(initTexture.width, initTexture.height);
         texture.filterMode = FilterMode.Point;
